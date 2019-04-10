b3d493e14d097d75bd36bcc48f628a48ba566cad
AlvinChen <AlvinChen@transglobe.com.tw>
2019-04-10 14:38:14 +0800

    調整

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@
 
     //取得清單
     function getOption() {
-        $.get("../docs/data.json", (response) => {
+        $.get("../doc/data.json", (response) => {
             datas = response;
             select.prepend($("<option></option>").attr("value", '全部單位').text('全部單位'));
             for (let i = 0; i < datas.length; i++) {
